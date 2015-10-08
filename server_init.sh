0159094b232d66ca7be8cdcd8866b34a9e351477
Andrew Hodges <betawaffle@gmail.com>
2015-10-07 20:06:10 -0400

    Add IRQ tuning to cloud-init for servers.

--- server_init.sh
+++ server_init.sh
@@ -8,6 +8,21 @@ net.ipv4.ip_local_port_range = 10000 65535
 EOF
 sysctl --system
 
+# Tune Network IRQ
+for i in $(seq 0 7); do
+  irq0=$(grep "eth0-${i}\b" /proc/interrupts | awk '{print $1}')
+  irq0=${irq0%:}
+  # echo -n "${irq0} " >> eth-affinity.log
+  # cat /proc/irq/${irq0}/smp_affinity >> eth-affinity.log
+  printf "%08x\n" $(( 1 << ($i * 2) )) > /proc/irq/${irq0}/smp_affinity
+
+  irq1=$(grep "eth1-${i}\b" /proc/interrupts | awk '{print $1}')
+  irq1=${irq0%:}
+  # echo -n "${irq1} " >> eth-affinity.log
+  # cat /proc/irq/${irq1}/smp_affinity >> eth-affinity.log
+  printf "%08x\n" $(( 1 << ($i * 2 + 16) )) > /proc/irq/${irq0}/smp_affinity
+done
+
 # Install System Packages
 apt-get install -y sudo htop sysstat jq
 
@@ -27,5 +42,16 @@ tar -xvf aerospike.tgz && cd aerospike-server
 private_ip=$(curl -s https://metadata.packet.net/metadata | jq -r '.network.addresses[2].address')
 echo "${config}" > etc/aerospike.conf
 
-# blkdiscard /dev/nvme0n1
-# blkdiscard /dev/nvme1n1
+# Write Reset Script
+cat > bin/reset <<EOF
+#!/bin/bash
+
+/root/aerospike-server/bin/aerospike stop
+mv /root/aerospike-server/var/log/aerospike.log /root/aerospike-server/var/log/aerospike.$(ls -l /root/aerospike-server/var/log/aerospike*.log | wc -l).log
+blkdiscard /dev/nvme0n1 &
+blkdiscard /dev/nvme1n1 &
+wait
+EOF
+
+# cd ~/aerospike-server
+# ./bin/reset && taskset -c 1-32:2 ./bin/aerospike start
